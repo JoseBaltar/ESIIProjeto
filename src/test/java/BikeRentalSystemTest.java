d6945372e82bd5be51f934b41e87dd930b6b96e5
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2018-11-17 18:31:05 +0000

    '15-22'

--- src/test/java/BikeRentalSystemTest.java
+++ src/test/java/BikeRentalSystemTest.java
@@ -1,5 +1,10 @@
+import Exceptions.UserAlreadyExists;
 import Exceptions.UserDoesNotExists;
+import Models.User;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.ThrowingSupplier;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 /**
  * Por enquanto só estão escritos os métodos de teste principais e
@@ -52,7 +57,18 @@ public class BikeRentalSystemTest {
     public void testVerifyCredit() {
 
         int IDUser;
+
+        //#TC15
         IDUser=23;
+        Assertions.assertTrue(brs.verifyCredit(IDUser),"Utilizador existe e crédito superior a 1");
+
+        //#TC16
+        IDUser=4;
+        Assertions.assertFalse(brs.verifyCredit(IDUser),"Utilizador existe e crédito inferior a 1");
+
+        //#TC17
+        IDUser=66;
+        Assertions.assertFalse(brs.verifyCredit(IDUser),"Utilizador não existe");
 
 
     }
@@ -61,7 +77,22 @@ public class BikeRentalSystemTest {
     public void testAddCredit() {
 
         int IDUser, amount;
+        User u=null;
+
+        //#TC18
         IDUser=55;amount=3;
+        Assertions.assertNotEquals(0,amount,"Amount diferente de 0, crédito");
+        Assertions.assertNotNull(IDUser,"User não é nulo e existe, atribuido");
+
+        //#TC19
+        amount=0;
+        Assertions.assertEquals(0,amount,"Amount igual a 0, não é atribuído crédito");
+        Assertions.assertNotNull(IDUser,"User não é nulo existe.");
+
+        //TC20
+        IDUser=66; amount=5;
+        Assertions.assertNotEquals(0,amount,"Amount diferente de 0");
+        Assertions.assertNull(u,"Utilizador é nulo, não é possível atribuir crédito");
 
     }
 
@@ -70,9 +101,25 @@ public class BikeRentalSystemTest {
 
         int IDUser, rentalprogram;
         String name;
+
+        //#TC21
         IDUser=55;rentalprogram=2;
         name="Sterben";
 
+        int finalIDUser = IDUser; int finalRentalprogram = rentalprogram;
+        String finalName = name;
+
+        Assertions.assertDoesNotThrow(()->brs.registerUser(finalIDUser, finalName, finalRentalprogram));
+
+       //#TC22
+        IDUser=66;rentalprogram=1;
+        name="Palpatine";
+
+        int finalIDUser1 = IDUser; int finalRentalprogram1 = rentalprogram;
+        String finalName1 = name;
+
+        Assertions.assertThrows(UserAlreadyExists.class,
+                () -> brs.registerUser(finalIDUser1, finalName1, finalRentalprogram1));
 
     }
 }
