f0fef194e5dc7eceb06903737598498a6c2ac423
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2018-11-18 16:58:58 +0000

    Mais métodos adicionados

--- src/test/java/BikeRentalSystemTest.java
+++ src/test/java/BikeRentalSystemTest.java
@@ -1,6 +1,7 @@
 import Exceptions.UserAlreadyExists;
 import Exceptions.UserDoesNotExists;
 import Models.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.ThrowingSupplier;
 
@@ -29,9 +30,40 @@ public class BikeRentalSystemTest {
         //Expressão relativa ao caso de teste com output:
         // -> UserDoesNotExists exception
         // (colocar o ID aqui)
-        int IDUser = 66, IDDeposit = 12, startTime = 12;
+       /* int IDUser = 66, IDDeposit = 12, startTime = 12;
         Assertions.assertThrows(UserDoesNotExists.class,
                 () -> brs.getBicycle(IDDeposit, IDUser, startTime));
+                Deixei estar por ser o original que foi feito
+                */
+        int IDUser, IDDeposit, starttime;
+
+
+        //#TC5
+        IDUser=55; IDDeposit=55; starttime=12;
+        //Como comparar em Assertions.assertEquals(), se não sabemos o IDlock que é suposto retornar? Rever amanhã
+
+        //#TC6
+        IDUser=66; IDDeposit=44; starttime=12;
+        int finalIDDeposit = IDDeposit, finalIDUser = IDUser, finalStarttime = starttime;
+        Assertions.assertThrows(UserDoesNotExists.class,
+                ()->brs.getBicycle(finalIDDeposit, finalIDUser, finalStarttime));
+
+        //#TC7
+        IDUser=23; IDDeposit=77; starttime=6;
+        Assertions.assertEquals(-1,brs.returnBicycle(IDDeposit,IDUser,starttime),"Depósito não existe, devia existir");
+
+        //#TC8
+        IDUser=66; IDDeposit=12; starttime=0;
+        Assertions.assertEquals(-1,brs.returnBicycle(IDDeposit,IDUser,starttime),"Saldo deveria ser superior a 1");
+
+        //#TC9
+        IDUser=55; IDDeposit=12; starttime=5;
+        Assertions.assertEquals(-1,brs.returnBicycle(IDDeposit,IDUser,starttime),"O Utilizador não deveria ter aluguer ativo");
+
+        //#TC10
+        IDUser=44; IDDeposit=0; starttime=12;
+        Assertions.assertEquals(-1,brs.returnBicycle(IDDeposit,IDUser,starttime),"Deveriam existir bicicletas disponíveis");
+
 
     }
 
@@ -39,7 +71,6 @@ public class BikeRentalSystemTest {
     public void testReturnBicycle() {
 
         int IDUser, IDDeposit, endtime;
-        IDUser=55; IDDeposit=55; endtime=12;
 
 
     }
@@ -47,9 +78,30 @@ public class BikeRentalSystemTest {
     @Test
     public void testBicycleRentalFee() {
 
-        //rentalProgram tem de ser igual ao construtor?
+    //estava a confundir o fee com o RentalProgram... Pronto, já percebi LMAO
 
-        int rentalProgram, starttime,endtime, nRentals;
+        int rentalProgram, starttime,endtime, nRentals, expectedResult;
+
+        //#TC11
+        rentalProgram=1;starttime=2;endtime=6; nRentals=3;
+        expectedResult=(endtime-starttime)*brs.getRentalFee();
+        Assertions.assertEquals(expectedResult,brs.bicycleRentalFee(rentalProgram,starttime,endtime,nRentals),"Mensagem");
+
+        //#TC12
+        rentalProgram=2;starttime=5;endtime=7; nRentals=6;
+        expectedResult=(endtime-starttime)*brs.getRentalFee();
+        Assertions.assertEquals(expectedResult,brs.bicycleRentalFee(rentalProgram,starttime,endtime,nRentals),"Mensagem");
+
+        //#TC13
+        rentalProgram=2;starttime=5;endtime=17; nRentals=9;
+        expectedResult=10*brs.getRentalFee()+((endtime-starttime)-10)*brs.getRentalFee()/2;
+        Assertions.assertEquals(expectedResult,brs.bicycleRentalFee(rentalProgram,starttime,endtime,nRentals),"Mensagem");
+
+        //#TC14
+        rentalProgram=2;starttime=5;endtime=12; nRentals=10;
+        expectedResult=0;
+        Assertions.assertEquals(expectedResult,brs.bicycleRentalFee(rentalProgram,starttime,endtime,nRentals),"Mensagem");
+
 
     }
 
